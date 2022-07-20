c92fcf80b58b1b51c5ecd826cf0c928ed93533ff
fangkai <1209683835@qq.com>
2022-07-20 17:08:41 +0800

    feat: 后台

--- route/app.php
+++ route/app.php
@@ -45,12 +45,8 @@ Route::group('api/:version/order', function() {
     Route::get('/paginate', 'api/:version.OrderController/getSummary')->allowCrossDomain(['Access-Control-Allow-Headers' => 'token']);
 });
 
-Route::group('api/:version/pay', function() {
-    Route::post('/pre_order', 'api/:version.PayController/getPreOrder');
-    Route::post('/notify', 'api/:version.PayController/receiveNotify');
-    Route::post('/re_notify', 'api/:version.PayController/redirectNotify');
-    Route::post('/concurrency', 'api/:version.PayController/notifyConcurrency');
-});
+Route::post('api/:version/pay/pre_order', 'api/:version.PayController/getPreOrder');
+Route::post('api/:version/pay/notify', 'api/:version.PayController/receiveNotify');
 
 Route::post('api/:version/address', 'api/:version.AddressController/createOrUpdateAddress');
 Route::get('api/:version/address', 'api/:version.AddressController/getUserAddress');
